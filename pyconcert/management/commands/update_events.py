3323ce9317d64680338113dfa174996792e41c89
Dominik <wahlholz@gmx.de>
2015-04-05 21:08:09 +0200

    Remove debug code.

--- pyconcert/management/commands/update_events.py
+++ pyconcert/management/commands/update_events.py
@@ -8,10 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         artists = [artist.name for artist in
                    Artist.objects.all()]
-        artists += ["Apocalyptica"]
-        print artists
         api_events = events_for_artists_bandsintown(artists, "Berlin")
-        print api_events
         for api_event in api_events:
             event, created = Event.objects.get_or_create(venue=api_event.venue,
                                                          city=api_event.city,
@@ -20,7 +17,5 @@ class Command(BaseCommand):
                                                          date=api_event.date,
                                                          time=api_event.time,
                                                          ticket_url=api_event.ticket_url)
-            print created
             if created:
                 event.save()
-        print "end"
