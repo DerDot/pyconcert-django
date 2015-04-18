354857f029887535d2a7ffb3b955db1ad145dc61
DerDot <wahlholz@gmx.de>
2015-04-18 16:02:28 +0200

    Signup page and email contents.

--- pyconcertproject/urls.py
+++ pyconcertproject/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^', include('pyconcert.urls', 'pyconcert')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login_view"),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r"^account/", include("account.urls")),
 )
