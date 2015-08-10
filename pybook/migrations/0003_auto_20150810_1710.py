bd1d6260cac23ffd40a1be20aad7fb0ea51e25b5
DoHe <wahlholz@gmx.de>
2015-08-10 17:41:08 +0200

    Clean migrations.

---
+++ pybook/migrations/0003_auto_20150810_1710.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pybook', '0002_auto_20150601_1659'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='book',
+            name='buy_url',
+            field=models.URLField(max_length=300),
+        ),
+        migrations.AlterField(
+            model_name='book',
+            name='title',
+            field=models.CharField(max_length=300),
+        ),
+    ]
