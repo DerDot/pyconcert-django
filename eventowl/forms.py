03f65b399a9b42e900ce34608fe470a89cf6c341
Dominik <wahlholz@gmx.de>
2015-08-17 21:15:02 +0200

    Move account handling to eventowl.
    Also fixed problem with city not working in settings/signup.

--- eventowl/forms.py
+++ eventowl/forms.py
@@ -1,6 +1,9 @@
 from django.template.defaultfilters import filesizeformat
 from django.core.exceptions import ValidationError
 from django import forms
+from django.contrib.auth.models import User
+from account import forms as account_forms
+
 
 class RestrictedFileField(forms.FileField):
 
@@ -25,3 +28,22 @@ class RestrictedFileField(forms.FileField):
             pass
 
         return data
+
+class SignupForm(account_forms.SignupForm):
+    city = forms.CharField(max_length=200)
+
+class SettingsForm(forms.Form):
+    city = forms.CharField(max_length=200)
+    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(SettingsForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(SettingsForm, self).clean()
+        email = cleaned_data.get("email")
+        if (email.lower() != self.user.email.lower() and
+            User.objects.filter(email__iexact=email).exists()):
+            raise ValidationError('E-mail already in use.')
+        return cleaned_data
--- pyconcert/urls.py
+++ pyconcert/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, url
 from pyconcert import views
 
 urlpatterns = patterns('',
-    url(r"^account/signup/$", views.SignupView.as_view(), name="account_signup"),
-    url(r"^account/settings/$", views.SettingsView.as_view(), name="account_settings"),
     url(r'^upload_json/$', views.upload_json, name='upload_json'),
     url(r'^provide_tool/$', views.ToolView.as_view(), name='provide_tool'),
     url(r'^spotify/$', views.spotify, name='spotify'),
