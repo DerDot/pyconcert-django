8a0a4faf739fce7a89504afaf02fc4e8b5f7c2ce
DoHe <wahlholz@gmx.de>
2015-09-28 13:37:24 +0200

    Fix celery result backend.

--- pyconcert/tasks.py
+++ pyconcert/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from celery import shared_task, current_task, task
 
 from models import Artist, RecommendedArtist
 from django.contrib.auth.models import User
--- pyconcertproject/celery.py
+++ pyconcertproject/celery.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import
 
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyconcertproject.settings')
-from django.conf import settings
 
 from celery import Celery
 
-app = Celery('pyconcertproject', broker='amqp://localhost')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyconcertproject.settings')
+
+from django.conf import settings
+
+
+app = Celery('pyconcertproject', broker='amqp://localhost', backend='djcelery.backends.database:DatabaseBackend')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(
     CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
 )
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
