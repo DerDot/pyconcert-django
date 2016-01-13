e60a6eadc89bbc75ddbabdc82d42066878dd9a13
DoHe <wahlholz@gmx.de>
2016-01-13 10:17:37 +0100

    Handle missing user agent

--- eventowl/utils/user_agents.py
+++ eventowl/utils/user_agents.py
@@ -2,10 +2,13 @@ from user_agents import parse
 
 
 def parse_user_agent(request):
-    ua_string = request.META['HTTP_USER_AGENT']
-    return parse(ua_string)
-    
+    try:
+        ua_string = request.META['HTTP_USER_AGENT']
+        return parse(ua_string)
+    except KeyError:
+        return None
+
 
 def is_robot(request):
     parsed = parse_user_agent(request)
-    return parsed.is_bot
+    return parsed is not None or parsed.is_bot
