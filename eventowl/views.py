89ec2a97fd1a2722ab4398c2c562a349d828eb55
DoHe <wahlholz@gmx.de>
2016-04-11 16:45:51 +0200

    Rely on heroku logging

--- eventowl/views.py
+++ eventowl/views.py
@@ -1,12 +1,8 @@
 from datetime import date, timedelta
 
-import os
-
-from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, ListView, FormView, View
 from account import views as account_views
 
@@ -18,8 +14,6 @@ from eventowlproject import settings
 from eventowl import app_previews
 from eventowl.utils.location import current_position
 from eventowl.utils.user_agents import is_robot
-from eventowl.utils.logging import get_log
-from eventowlproject.settings import LOG_DIRECTORY
 
 
 class ChoiceView(TemplateView):
@@ -212,27 +206,6 @@ class SettingsView(FormView):
         kwargs = super(SettingsView, self).get_form_kwargs()
         kwargs.update({'user': self.request.user})
         return kwargs
-
-
-@method_decorator(user_passes_test(lambda u: u.is_superuser), 'dispatch')
-class LogDirectoryView(TemplateView):
-    template_name = 'eventowl/log_directory.html'
-
-    def get_context_data(self, **kwargs):
-        log_names = os.listdir(LOG_DIRECTORY)
-        kwargs['log_paths'] = [os.path.join(LOG_DIRECTORY, log_name) for log_name in log_names]
-        return super().get_context_data(**kwargs)
-
-
-@method_decorator(user_passes_test(lambda u: u.is_superuser), 'dispatch')
-class LogView(TemplateView):
-    template_name = 'eventowl/log_viewer.html'
-
-    def get_context_data(self, **kwargs):
-        log_path = self.request.GET['log_path']
-        log = get_log(log_path)
-        kwargs['log'] = log
-        return super().get_context_data(**kwargs)
 
 
 class ICalView(View):
