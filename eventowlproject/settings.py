b19717a34a16b5b421dc102080dc03ab04f5ead4
DoHe <wahlholz@gmx.de>
2017-02-19 23:30:51 +0100

    Change log level on production to INFO

--- eventowlproject/settings.py
+++ eventowlproject/settings.py
@@ -182,7 +182,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'DEBUG' if DEBUG else 'INFO',
             'propagate': True,
             },
         }
