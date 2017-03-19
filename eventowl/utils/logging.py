9a07a00112c285ffbcff5ea229a3cc6531750261
DoHe <wahlholz@gmx.de>
2017-03-19 13:45:33 +0100

    Remove unused utils

--- eventowl/utils/logging.py
+++
@@ -1,5 +0,0 @@
-def get_log(log_path, number_lines=100):
-    with open(log_path) as log_file:
-        lines = log_file.readlines()
-    last_lines = lines[-number_lines:]
-    return ''.join(last_lines)
