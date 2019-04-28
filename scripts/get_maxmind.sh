b17754e4ee5b3944cc4d4f98334ff8ac0300e6af
dohe <wahlholz@gmx.de>
2019-04-28 17:38:49 +0200

    Use local download dir

--- scripts/get_maxmind.sh
+++ scripts/get_maxmind.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget -O db.tar.gz https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
-tar -C /tmp -zxvf db.tar.gz
-mv /tmp/GeoLite2-City_*/GeoLite2-City.mmdb vendor/maxmind.mmdb
+mkdir -p .tmp/
+tar -C .tmp/ -zxvf db.tar.gz
+mv .tmp/GeoLite2-City_*/GeoLite2-City.mmdb vendor/maxmind.mmdb
 rm db.tar.gz
-rm -r /tmp/GeoLite2-City_*
+rm -r .tmp/
