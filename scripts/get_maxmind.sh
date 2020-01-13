78edc96d35d6a19ef8a577f382cdba3818a60df8
dohe <wahlholz@gmx.de>
2020-01-13 11:21:36 +0100

    New style maxmind download

--- scripts/get_maxmind.sh
+++ scripts/get_maxmind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O db.tar.gz https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+wget -O db.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${MAXMIND_KEY}&suffix=tar.gz"
 mkdir -p .tmp/
 tar -C .tmp/ -zxvf db.tar.gz
 mv .tmp/GeoLite2-City_*/GeoLite2-City.mmdb vendor/maxmind.mmdb
