c846a585f04378166b7a0b1448add7159b5dc352
dohe <dominik.henter@ecosia.org>
2021-05-22 09:22:09 +0200

    Use TLS URL

--- eventowlproject/celery.py
+++ eventowlproject/celery.py
@@ -1,12 +1,12 @@
+from django.conf import settings
 import os
 
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventowlproject.settings')
 
-from django.conf import settings
 
-BROKER_URL = os.environ.get('REDIS_URL', 'localhost')
+BROKER_URL = os.environ.get('REDIS_TLS_URL', 'localhost')
 
 app = Celery('eventowlproject', broker=BROKER_URL,
              backend='djcelery.backends.database:DatabaseBackend')
